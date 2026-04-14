7d7aa016dc2529f24efb741b4c2852947b79e79b
Muhamad Rizki Aditya <muhamadrizkiaditya32@gmail.com>
2026-04-14 23:13:18 +0700

    setup project express js

---
+++ backend/app.js
@@ -0,0 +1,43 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// router
+const categoriesRouter = require("./app/api/v1/categories/router");
+const v1 = "/api/v1/cms";
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.use(v1, categoriesRouter);
+
+app.use("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to API Semina",
+  });
+});
+
+module.exports = app;

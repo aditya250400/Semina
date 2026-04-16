31bddbc31f9b50529baa45860ff45ee9b2e9eba8
Muhamad Rizki Aditya <muhamadrizkiaditya32@gmail.com>
2026-04-16 21:04:56 +0700

    configure mongodb and mongoose

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,14 @@ app.use(express.static(path.join(__dirname, "public")));
 const categoriesRouter = require("./app/api/v1/categories/router");
 const v1 = "/api/v1/cms";
 
+app.use(v1, categoriesRouter);
+
+app.use("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to API Semina",
+  });
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -23,20 +31,9 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-app.use(v1, categoriesRouter);
-
-app.use("/", (req, res) => {
-  res.status(200).json({
-    message: "Welcome to API Semina",
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err : {},
   });
 });
 

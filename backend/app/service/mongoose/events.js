02723182112ac39e7490dd5eb9bc6ec3b570bddc
Muhamad Rizki Aditya <muhamadrizkiaditya32@gmail.com>
2026-04-29 00:24:13 +0700

    add status query paras in events

--- backend/app/service/mongoose/events.js
+++ backend/app/service/mongoose/events.js
@@ -5,7 +5,7 @@ const { checkingTalents } = require("./talents");
 const { NotFoundError, BadRequestError } = require("../../errors");
 
 const getAllEvents = async (req) => {
-  const { keyword, category, talent } = req.query;
+  const { keyword, category, talent, status } = req.query;
 
   let condition = { organizer: req.user.organizer };
 
@@ -19,6 +19,10 @@ const getAllEvents = async (req) => {
 
   if (talent) {
     condition = { ...condition, talent };
+  }
+
+  if (["Draft", "Published"].includes(status)) {
+    condition = { ...condition, statusEvent: status };
   }
 
   const result = await Events.find(condition)
